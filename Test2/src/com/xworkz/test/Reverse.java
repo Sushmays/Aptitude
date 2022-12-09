a610b1adc07e0e3dd4a37941c994835fb910a510
RPS YS <sushmaysxworkz@gamil.com>
2022-12-09 13:13:41 +0530

    test

---
+++ Test2/src/com/xworkz/test/Reverse.java
@@ -0,0 +1,19 @@
+package com.xworkz.test;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+
+		String str= "practice";
+			
+		int i=str.length();
+		while(i>0) {
+		System.out.println(str.charAt(i-1));
+		i--;	
+		}
+		
+	}
+	
+
+}
+
